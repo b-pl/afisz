18cae9c39a98b2c94736ca2546d41c08983185ad
b-pl <bartoszrydzewski@outlook.com>
2019-07-14 20:47:39 +0200

    feat(router): Install hookrouter
    Setup routing:
    - Homepage is avaible under /
    - OffersList is avaible under /offers
    - Add content in index.js, App.jsx, Routes.jsx, Homepage.jsx,
       OffersList.jsx

---
+++ webapp/src/components/OffersList/OffersList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class OffersList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>
+        <p>This is OffersList Component</p>
+      </div>
+    )
+  }
+}
+
+export default OffersList
---
+++ webapp/src/core/Routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Homepage from '../components/Homepage/Homepage'
+import OffersList from '../components/OffersList/OffersList'
+
+const routes = {
+  "/": () => <Homepage />,
+  "/offers": () => <OffersList />
+};
+
+export default routes;
