81887d3802642f74a09ab45ab1551b033393a020
b-pl <bartoszrydzewski@outlook.com>
2019-07-21 16:20:10 +0200

    feat(OffersList): render offers dynamically

--- webapp/src/components/OffersList/OffersList.jsx
+++ webapp/src/components/OffersList/OffersList.jsx
@@ -4,26 +4,46 @@ import Offer from '../Offer/Offer'
 class OffersList extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      offer: [
+        {
+          title: 'Huawei P20 Pro Blue [NEW]',
+          price: '599,99',
+          category: 'Electronics',
+          date: '13.07.19'
+        },
+        {
+          title: 'Uranium 1kg',
+          price: '49,99',
+          category: 'Elements',
+          date: '17.07.19'
+        },
+        {
+          title: 'Rammstein - Rammstein',
+          price: '19,99',
+          category: 'Music',
+          date: '17.07.19'
+        },
+        {
+          title: 'Steven Seagal picture',
+          price: '9,99',
+          category: 'Art',
+          date: '21.07.19'
+        }
+      ]
+    }
   }
 
   render() {
     return(
       <div>
-        <Offer 
-          title='Huawei P20 Pro Blue [NEW]'
-          price='599,99'
-          category='Electronics'
-          date='13.07.19' />
-        <Offer
-          title='Uranium 1kg'
-          price='49,99'
-          category='Elements'
-          date='17.07.19' />
-        <Offer
-          title='Rammstein - Rammstein'
-          price='19,99'
-          category='Music'
-          date='17.07.19' />
+        {this.state.offer.map(offer => (
+          <Offer 
+            title={offer.title}
+            price={offer.price}
+            category={offer.category}
+            date={offer.date} />
+        ))}
       </div>
     )
   }
