dfb02164b5df44099aa87c3c420746df0efab3c3
b-pl <bartoszrydzewski@outlook.com>
2019-08-05 18:31:28 +0200

    feat(OfferDetails): Create OfferDetails components
    - Created component
    - Component Accessibility (via URL & OffersList)
    - Syntax fixes (eslint errors)

--- webapp/src/components/OfferDetails/OfferDetails.jsx
+++ webapp/src/components/OfferDetails/OfferDetails.jsx
@@ -1,25 +1,52 @@
 import React from 'react'
 
 class OfferDetails extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor (props) {
+    super (props)
     this.state = {
       offer: [
         {
+          offerID: 0,
           title: 'Huawei P20 Pro Blue [NEW]',
           price: '599,99',
           category: 'Electronics',
           date: '13.07.19',
-          offerID: 0
+          description: ''
+        },
+        {
+          offerID: 1,
+          title: 'Uranium 1kg',
+          price: '49,99',
+          category: 'Elements',
+          date: '17.07.19',
+          description: ''
+        },
+        {
+          offerID: 2,
+          title: 'Rammstein - Rammstein',
+          price: '19,99',
+          category: 'Music',
+          date: '17.07.19',
+          description: ''
+        },
+        {
+          offerID: 3,
+          title: 'Steven Seagal picture',
+          price: '9,99',
+          category: 'Art',
+          date: '21.07.19',
+          description: ''
         }
       ]
     }
   }
 
-  render() {
+  render () {
+    const id = this.props.offerID
     return (
       <div className='details' offerID={this.props.offerID}>
-        {this.props.offerID}
+        <p>offerID: {this.props.offerID}</p>
+        <p>Title: {this.state.offer[id].title}</p>
       </div>
     )
   }
--- webapp/src/components/OffersList/OffersList.jsx
+++ webapp/src/components/OffersList/OffersList.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Offer from '../Offer/Offer'
 
 class OffersList extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor (props) {
+    super (props)
     this.state = {
       offer: [
         {
@@ -18,35 +18,36 @@ class OffersList extends React.Component {
           price: '49,99',
           category: 'Elements',
           date: '17.07.19',
-          offerID: 2
+          offerID: 1
         },
         {
           title: 'Rammstein - Rammstein',
           price: '19,99',
           category: 'Music',
           date: '17.07.19',
-          offerID: 3
+          offerID: 2
         },
         {
           title: 'Steven Seagal picture',
           price: '9,99',
           category: 'Art',
           date: '21.07.19',
-          offerID: 4
+          offerID: 3
         }
       ]
     }
   }
 
-  render() {
-    return(
+  render () {
+    return (
       <div>
         {this.state.offer.map(offer => (
-            <Offer 
-              title={offer.title}
-              price={offer.price}
-              category={offer.category}
-              date={offer.date} /> 
+          <Offer
+            title={offer.title}
+            price={offer.price}
+            category={offer.category}
+            date={offer.date}
+            offerID={offer.offerID} />
         ))}
       </div>
     )
