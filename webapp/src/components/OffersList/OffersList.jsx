3f578d157d7fe85fc936cd2dc113935166016a0c
b-pl <bartoszrydzewski@outlook.com>
2020-04-11 10:45:11 +0200

    See description
    I'm picking up this project after a while.
    I'm commiting everything not commited yet.

--- webapp/src/components/OffersList/OffersList.jsx
+++ webapp/src/components/OffersList/OffersList.jsx
@@ -7,7 +7,56 @@ class OffersList extends React.Component {
     super (props)
     this.state = {
       offers: [],
+      categories: ['Art & Antiques',
+        'Books',
+        'Cell Phones',
+        'Computers',
+        'Consumer Eletronics',
+        'Health & Beauty',
+        'Home & Garden',
+        'Jewelry',
+        'Movies',
+        'Music',
+        'Pets',
+        'Sporting Goods',
+        'Toys',
+        'Video Games & Consoles'],
+        selectedOffers: [],
+        selectedCategory: 'Music'
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange(e){
+    this.setState({ selectedCategory: e.target.value })
+    const target = e.target.value
+
+    Object.keys(this.state.offers).forEach((offer => 
+      // console.log(this.state.offers[offer]))) 
+      this.state.offers[offer].category === target
+      ? this.setState({ selectedOffers: [...this.state.selectedOffers, this.state.offers[offer]] })
+      : console.log("Error :(")))
+  }
+
+  showOffers() {
+    return () => this.state.selectedOffers.length === '0'
+          ? this.state.offers && this.state.offers.map(offer => (
+            <Offer
+              title={offer.title}
+              price={offer.price}
+              category={offer.category}
+              date={offer.date}
+              offerID={offer.id} />
+          ))
+          : this.state.selectedOffers && this.state.selectedOffers.map(offer => (
+            <Offer
+              title={offer.title}
+              price={offer.price}
+              category={offer.category}
+              date={offer.date}
+              offerID={offer.id} />
+          ))
   }
 
   componentDidMount() {
@@ -15,16 +64,39 @@ class OffersList extends React.Component {
       accept: 'application/json',
     })
       .then(res => res.json())
-      .then(data => {
-        const offers = data
+      .then(offers => {
         this.setState({offers})
       })  
   }
 
-  render () {
+  render() {
     return (
       <div>
-        {this.state.offers && this.state.offers.map( offer => (
+        <select name='category' onChange={this.handleInputChange}>{this.state.categories && this.state.categories.map(category => (
+          <option value={category}>{category}</option>
+        ))}</select>
+
+        {/* {this.showOffers()} */}
+
+       {/* {() => this.state.selectedOffers.length === '0'
+          ? this.state.offers && this.state.offers.map(offer => (
+            <Offer
+              title={offer.title}
+              price={offer.price}
+              category={offer.category}
+              date={offer.date}
+              offerID={offer.id} />
+          ))
+          : this.state.selectedOffers && this.state.selectedOffers.map(offer => (
+            <Offer
+              title={offer.title}
+              price={offer.price}
+              category={offer.category}
+              date={offer.date}
+              offerID={offer.id} />
+          ))
+         } */}
+        { this.state.offers && this.state.offers.map(offer => (
           <Offer
             title={offer.title}
             price={offer.price}
@@ -36,5 +108,6 @@ class OffersList extends React.Component {
     )
   }
 }
+
 
 export default OffersList
