f8edd6a77d3a00ecaeabe55c61690d57e4cb43f8
b-pl <bartoszrydzewski@outlook.com>
2020-04-22 18:55:29 +0200

    Refactor(OffersList): stateless component

--- webapp/src/components/OffersList/OffersList.jsx
+++ webapp/src/components/OffersList/OffersList.jsx
@@ -1,113 +1,48 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Offer from '../Offer/Offer'
 import host from '../../core/config'
 
-class OffersList extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      offers: [],
-      categories: ['Art & Antiques',
-        'Books',
-        'Cell Phones',
-        'Computers',
-        'Consumer Eletronics',
-        'Health & Beauty',
-        'Home & Garden',
-        'Jewelry',
-        'Movies',
-        'Music',
-        'Pets',
-        'Sporting Goods',
-        'Toys',
-        'Video Games & Consoles'],
-        selectedOffers: [],
-        selectedCategory: 'Music'
-    }
+function OffersList() {
+  const [categories] = useState(['Art & Antiques',
+    'Books',
+    'Cell Phones',
+    'Computers',
+    'Consumer Eletronics',
+    'Health & Beauty',
+    'Home & Garden',
+    'Jewelry',
+    'Movies',
+    'Music',
+    'Pets',
+    'Sporting Goods',
+    'Toys',
+  'Video Games & Consoles'])
+  const [offers, setOffers] = useState([])
 
-    this.handleInputChange = this.handleInputChange.bind(this)
-  }
-
-  handleInputChange(e){
-    this.setState({ selectedCategory: e.target.value })
-    const target = e.target.value
-
-    Object.keys(this.state.offers).forEach((offer => 
-      // console.log(this.state.offers[offer]))) 
-      this.state.offers[offer].category === target
-      ? this.setState({ selectedOffers: [...this.state.selectedOffers, this.state.offers[offer]] })
-      : console.log("Error :(")))
-  }
-
-  showOffers() {
-    return () => this.state.selectedOffers.length === '0'
-          ? this.state.offers && this.state.offers.map(offer => (
-            <Offer
-              title={offer.title}
-              price={offer.price}
-              category={offer.category}
-              date={offer.date}
-              offerID={offer.id} />
-          ))
-          : this.state.selectedOffers && this.state.selectedOffers.map(offer => (
-            <Offer
-              title={offer.title}
-              price={offer.price}
-              category={offer.category}
-              date={offer.date}
-              offerID={offer.id} />
-          ))
-  }
-
-  componentDidMount () {
+  useEffect(() => {
     fetch(`${host}/offers_list`, {
       accept: 'application/json'
     })
       .then(res => res.json())
       .then(offers => {
-        this.setState({offers})
-      })  
-  }
+        setOffers(offers)
+      })
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <select name='category' onChange={this.handleInputChange}>{this.state.categories && this.state.categories.map(category => (
-          <option value={category}>{category}</option>
-        ))}</select>
 
-        {/* {this.showOffers()} */}
-
-       {/* {() => this.state.selectedOffers.length === '0'
-          ? this.state.offers && this.state.offers.map(offer => (
-            <Offer
-              title={offer.title}
-              price={offer.price}
-              category={offer.category}
-              date={offer.date}
-              offerID={offer.id} />
-          ))
-          : this.state.selectedOffers && this.state.selectedOffers.map(offer => (
-            <Offer
-              title={offer.title}
-              price={offer.price}
-              category={offer.category}
-              date={offer.date}
-              offerID={offer.id} />
-          ))
-         } */}
-        { this.state.offers && this.state.offers.map(offer => (
-          <Offer
-            title={offer.title}
-            price={offer.price}
-            category={offer.category}
-            date={offer.date}
-            offerID={offer.id} />
-        ))}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {offers && offers.map(offer => (
+        <Offer
+          title={offer.title}
+          price={offer.price}
+          category={offer.category}
+          date={offer.date}
+          offerID={offer.id} />
+      ))}
+    </div>
+  )
 }
-
 
 export default OffersList
